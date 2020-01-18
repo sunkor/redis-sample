e5e64ef14501db7bc073caba2497d3cd30182ae7
Sunil Dhummi <sunil.dhummi@thetradedesk.com>
2020-01-18 16:29:08 +1100

    async redis

--- git-repo-fetcher/index.js
+++ git-repo-fetcher/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const fetch = require("node-fetch");
-const redis = require("redis");
+const asyncRedis = require("async-redis");
 
 const PORT = process.env.PORT || 8080;
 const gitApiEndpoint = `https://api.github.com/users`;
 
 const app = express();
 
-const redisClient = redis.createClient({
+const asyncRedisClient = asyncRedis.createClient({
   host: process.env.REDIS_HOST || "redis",
   port: process.env.REDIS_PORT || 6379
 });
 
 function setResponse(username, repos) {
-  return `<h2>${username} has ${repos} Github repos</h2>`;
+  return `<h2>${username} has ${
+    JSON.parse(repos).repo_count
+  } Github repos</h2>`;
 }
 
 async function getRepos(req, res, next) {
@@ -23,10 +25,12 @@ async function getRepos(req, res, next) {
     const { username } = req.params;
     const response = await fetch(`${gitApiEndpoint}/${username}`);
     const data = await response.json();
-    const repos = data.public_repos;
+    const repos = JSON.stringify({
+      repo_count: data.public_repos
+    });
 
     //Set data to Redis.
-    redisClient.setex(username, 3600, repos);
+    await asyncRedisClient.setex(username, 3600, repos);
 
     res.send(setResponse(username, repos));
   } catch (err) {
@@ -35,20 +39,16 @@ async function getRepos(req, res, next) {
   }
 }
 
-function cache(req, res, next) {
+async function cache(req, res, next) {
   const { username } = req.params;
 
-  redisClient.get(username, (err, data) => {
-    if (err) throw err;
-
-    var ignoreCache = req.query.ignoreCache == "true";
-
-    if (data && !ignoreCache) {
-      res.send(setResponse(username, data));
-    } else {
-      next();
-    }
-  });
+  const repos = await asyncRedisClient.get(username);
+  const ignoreCache = req.query.ignoreCache == "true";
+  if (repos && !ignoreCache) {
+    res.send(setResponse(username, repos));
+  } else {
+    next();
+  }
 }
 
 app.get("/repos/:username", cache, getRepos);
